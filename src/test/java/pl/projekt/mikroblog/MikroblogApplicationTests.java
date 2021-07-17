5e2d4bfbd6999f450612159de731d4fdaa835961
Kacper <kacpermielnikit@gmail.com>
2021-07-17 12:18:17 +0200

    Initialize commit

---
+++ src/test/java/pl/projekt/mikroblog/MikroblogApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.projekt.mikroblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikroblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
