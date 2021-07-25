1254e9d822f6ad779db568723f5abfd649fb7fa9
Piotr Białobrzeski <p.d.bialobrzeski@gmail.com>
2021-07-25 15:49:29 +0200

    Delete single comment functionality

--- src/main/java/pl/projekt/mikroblog/post/service/CommentService.java
+++ src/main/java/pl/projekt/mikroblog/post/service/CommentService.java
@@ -8,5 +8,8 @@ import java.util.List;
 public interface CommentService {
 
     List<Comment> findAllCommentsByPost(long id);
+
     void deleteCommentByPostID(long id);
+
+    void deleteCommentByID(long id);
 }
