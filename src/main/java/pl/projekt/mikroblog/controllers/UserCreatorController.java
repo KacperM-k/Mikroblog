d65dc47b670a4aff6eadd882f540fd87c6a60000
GrazynaGor <Grewazaza11@>
2021-08-01 11:00:53 +0200

    User Login

--- src/main/java/pl/projekt/mikroblog/controllers/UserCreatorController.java
+++ src/main/java/pl/projekt/mikroblog/controllers/UserCreatorController.java
@@ -5,11 +5,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.projekt.mikroblog.service.UserService;
 import pl.projekt.mikroblog.user.User;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Date;
 
@@ -17,13 +20,13 @@ import java.util.Date;
 public class UserCreatorController {
 
     UserService userService;
-
+    @Autowired
     PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserCreatorController(UserService userService,PasswordEncoder passwordEncoder) {
+    public UserCreatorController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
-        this.passwordEncoder=passwordEncoder;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/account/new")
@@ -33,18 +36,19 @@ public class UserCreatorController {
     }
 
     @PostMapping("/new_user")
-    public String addNewUserAccount(@Valid User user) {
-        for (User user1 : userService.findAllUsers()) {
-            if (user.getLogin().equals(user1.getLogin()))
-                return "User is not unique!";
-        } // do poprawki
+    public String addNewUserAccount(@ModelAttribute("user") @Valid User user, HttpServletRequest request, Errors errors) {
+        try {
+            userService.addUser(user);
+            Date date = new Date();
+            user.setCreateAccountDate(date);
+            user.setStatus("Active");
+            user.setTyp("user");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        Date date = new Date();
-        user.setCreateAccountDate(date);
-        user.setStatus("Active");
-        user.setTyp("user");
-     //   user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userService.addUser(user);
+        //   user.setPassword(passwordEncoder.encode(user.getPassword()));
+        // userService.addUser(user);
         return "redirect:/login";
     }
 }
